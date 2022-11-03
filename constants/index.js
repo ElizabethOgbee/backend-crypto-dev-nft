de3950c1ee7926a7f01bd31f051dc747978c8738
ElizabethOgbee <ogbeititi345@gmail.com>
2022-11-03 22:44:09 +0100

    Initial Commit

---
+++ constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xEc51A684b0223D5CD8159250c173681e5E34F026";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
